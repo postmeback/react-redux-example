20a5c4b1c3ffa068ea4147f8e23e5153b50da58a
postmeback <poddararka27@gmail.com>
2023-07-05 16:04:59 +0530

    Error Rectified

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -2,37 +2,33 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   data: [],
-  status : 'idle'
+  status: "idle",
 };
+
+export const setProducts = createAsyncThunk("products/get", async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const result = await response.json();
+  return result;
+});
 
 const productSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {
-  },
+  reducers: {},
 
-  extraReducers : (builder) => {
+  extraReducers: (builder) => {
     builder
-    .addCase(setProducts.pending, (state, action) => {
-state.status = 'Loading';
-    })
-    .addCase(setProducts.fulfilled, (state, action) => {
+      .addCase(setProducts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(setProducts.fulfilled, (state, action) => {
         state.data = action.payload;
-        state.status = 'idle';
-    })
-    .addCase(setProducts.rejected, (state, action) => {
-        state.status = 'error'
-    })
-  }
+        state.status = "idle";
+      })
+      .addCase(setProducts.rejected, (state, action) => {
+        state.status = "error";
+      });
+  },
 });
 
-// export const { fetchProducts } = productSlice.actions;
 export default productSlice.reducer;
-
-export const setProducts = createAsyncThunk('products/get', async () => {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const result = await response.json();
-    return result;
-})
-
-  
