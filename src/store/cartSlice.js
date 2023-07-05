926a1deffc0ffdd8aa95cedd228121eaecb114c9
postmeback <poddararka27@gmail.com>
2023-07-05 15:13:54 +0530

    Buttons Implemented, Count to manage

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -3,20 +3,31 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [];
 
 const cartSlice = createSlice({
-    name : 'cart',
-    initialState,
-    reducers: {
-        add(state, action) {
-            state.push(action.payload)       
-        },
+  name: "cart",
+  initialState,
+  reducers: {
+    add(state, action) {
+      const { id } = action.payload;
+      const existingItem = state.find((item) => item.id === id);
+      if (existingItem) {
+        existingItem.count += 1;
+      } else {
+        state.push({ ...action.payload, count: 1 });
+      }
+    },
 
-        remove(state, action){
-            return state.filter(item => item.id !== action.payload)
-        }
-    }
+    remove(state, action) {
+      const { id } = action.payload;
+
+      const existingItem = state.find((item) => item.id === id);
+      if (existingItem) {
+        return state.filter((item) => item.id !== id);
+      } else {
+        existingItem.count -= 1;
+      }
+    },
+  },
 });
 
-
-
-export const {add, remove} = cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
 export default cartSlice.reducer;
